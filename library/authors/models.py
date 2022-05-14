9ac2211367a934dccdeb67141e0076079b9b8853
Sizoran <89024848835@bk.ru>
2022-05-14 14:30:37 +0300

    Fist commit. Models add

---
+++ library/authors/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    birthday_year = models.PositiveIntegerField()
+    email = models.CharField(max_length=64, unique=True)
